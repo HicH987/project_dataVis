45436af72fac93e665deac6efc58cc40ca696d80
HicH987 <diabhicham987@gmail.com>
2022-12-23 21:21:26 +0100

    make renderSchedule more shorter

--- src/handlers/mapRenderResult.js
+++ src/handlers/mapRenderResult.js
@@ -7,155 +7,159 @@ import { glob } from "../global/var";
 
 let oldCourData = {};
 let oldGroupsData = {};
+let oldEventData = {};
 
-export function renderSchedule(mapData, courData, groupsData, from) {
-  const g = d3.select(`.${gClass}`);
+const filtreMapByGResult = (currentMapData, groupsData) => {
+  return Object.values(groupsData).filter(
+    (o) =>
+      o.bat === currentMapData.properties.bat &&
+      o.loc === currentMapData.properties[o.floor]
+  )[0];
+};
+const isCourData = (data) => (data.time ? true : false);
 
-  if (!mapData.loading) {
-    if (courData && !isEqual(courData, oldCourData)) {
-      if (!from) g.selectAll(".toDlt").remove();
+const renderInfoBubble = (currentData, eventData) => {
+  if (glob.zoomScale <= 3) return;
 
-      var crMapData = filtreMapDataBy.cour(mapData, courData);
-      console.log("crMapDatadddd", crMapData);
-      g.append("g")
-        .selectAll("path")
-        .data(crMapData)
-        .enter()
-        .append("path")
-        .attr("class", "toDlt")
-        .attr("d", glob.path)
-        .style("fill", "#4694DD")
-        .style("stroke-width", "0.1")
-        .style("stroke", "black")
-        .on("mouseover", function (_, d) {
-          if (glob.zoomScale > 3) {
-            d3
-              .select(".classtip")
-              .style("visibility", "visible")
-              .append("div")
-              .attr("class", "tip").html(`
+  let data = eventData;
+  let evnt = "Course";
+
+  if (!isCourData(eventData)) {
+    data = filtreMapByGResult(currentData, eventData);
+    evnt = data.e;
+  }
+
+  d3
+    .select(".classtip")
+    .style("visibility", "visible")
+    .append("div")
+    .attr("class", "tip").html(`
               <table class='tab'>
               <tbody>
                 <tr>
-                  <td>Course</td>
-                  <td>${courData.sub}</td>
+                  <td>${evnt}</td>
+                  <td>${data.sub}</td>
                 </tr>
                 <tr>
                   <td>Prof</td>
-                  <td>${courData.prof}</td>
+                  <td>${data.prof}</td>
                 </tr>
                 <tr>
                   <td>Time</td>
-                  <td>${courData.time}</td>
+                  <td>${data.time}</td>
                 </tr>
                 <tr>
                   <td>Day</td>
-                  <td>${courData.day}</td>
+                  <td>${data.day}</td>
                 </tr>
                 <tr>
                   <td>Building</td>
-                  <td>${courData.bat}</td>
+                  <td>${data.bat}</td>
                 </tr>
                 <tr>
-                  <td>Floor ${courData.floor.charAt(
-                    courData.floor.length - 1
-                  )} </td>
+                  <td>Floor ${data.floor.charAt(data.floor.length - 1)} </td>
                   <td>
-                    ${courData.loc}
+                    ${data.loc}
                   </td>
                 </tr>
               </tbody>
             </table>
               `);
-          }
-        })
-        .on("mousemove", function () {
-          d3.select(".classtip")
-            .style("top", event.pageY - 10 + "px")
-            .style("left", event.pageX + 170 + "px");
-        })
-        .on("mouseout", function () {
-          d3.select(".classtip").selectAll(".tip").remove();
-          d3.select(".classtip").style("visibility", "hidden");
-        });
+};
 
-      oldCourData = courData;
-    }
-    if (groupsData && !isEqual(groupsData, oldGroupsData)) {
-      if (!from) g.selectAll(".toDlt").remove();
-      var grMapData = filtreMapDataBy.groups(mapData, groupsData);
+/*export function renderSchedule(mapData, courData, groupsData, from) {
+  if (mapData.loading) return;
 
-      g.append("g")
-        .selectAll("path")
-        .data(grMapData)
-        .enter()
-        .append("path")
-        .attr("class", "toDlt")
-        .attr("d", glob.path)
-        .style("fill", "#4694DD")
-        .style("stroke-width", "0.1")
-        .style("stroke", "black")
-        .on("mouseover", function (_, d) {
-          if (glob.zoomScale > 3) {
-            let arr = Object.keys(groupsData).map((k) => groupsData[k]);
-            let currentD = arr.filter(
-              (o) =>
-                o.bat === d.properties.bat && o.loc === d.properties[o.floor]
-            )[0];
-            console.log("currentD", currentD);
-            d3.select(".classtip")
-              .style("visibility", "visible")
-              .append("div")
-              .attr("class", "tip")
-              .html(
-                `
-              <table>
-              <tbody>
-                <tr>
-                  <td>${currentD.e}</td>
-                  <td>${currentD.sub}</td>
-                </tr>
-                <tr>
-                  <td>Prof</td>
-                  <td>${currentD.prof}</td>
-                </tr>
-                <tr>
-                  <td>Time</td>
-                  <td>${currentD.time}</td>
-                </tr>
-                <tr>
-                  <td>Day</td>
-                  <td>${currentD.day}</td>
-                </tr>
-                <tr>
-                  <td>Building</td>
-                  <td>${currentD.bat}</td>
-                </tr>
-                <tr>
-                  <td>Floor ${currentD.floor.substr(
-                    currentD.floor.length - 1
-                  )} </td>
-                  <td>
-                    ${currentD.loc}
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-              `
-              );
-          }
-        })
-        .on("mousemove", function () {
-          d3.select(".classtip")
-            .style("top", event.pageY - 10 + "px")
-            .style("left", event.pageX + 170 + "px");
-        })
-        .on("mouseout", function () {
-          d3.select(".classtip").selectAll(".tip").remove();
-          d3.select(".classtip").style("visibility", "hidden");
-        });
+  const g = d3.select(`.${gClass}`);
 
-      oldGroupsData = groupsData;
-    }
+  if (courData && !isEqual(courData, oldCourData)) {
+    if (!from) g.selectAll(".toDlt").remove();
+
+    var crMapData = filtreMapDataBy.cour(mapData, courData);
+    g.append("g")
+      .selectAll("path")
+      .data(crMapData)
+      .enter()
+      .append("path")
+      .attr("class", "toDlt")
+      .attr("d", glob.path)
+      .style("fill", "#4694DD")
+      .style("stroke-width", "0.1")
+      .style("stroke", "black")
+      .on("mouseover", (_, d) => renderInfoBubble(d, courData))
+      .on("mousemove", () => {
+        d3.select(".classtip")
+          .style("top", event.pageY - 10 + "px")
+          .style("left", event.pageX + 170 + "px");
+      })
+      .on("mouseout", () => {
+        d3.select(".classtip").selectAll(".tip").remove();
+        d3.select(".classtip").style("visibility", "hidden");
+      });
+
+    oldCourData = courData;
+  }
+  if (groupsData && !isEqual(groupsData, oldGroupsData)) {
+    if (!from) g.selectAll(".toDlt").remove();
+    var grMapData = filtreMapDataBy.groups(mapData, groupsData);
+
+    g.append("g")
+      .selectAll("path")
+      .data(grMapData)
+      .enter()
+      .append("path")
+      .attr("class", "toDlt")
+      .attr("d", glob.path)
+      .style("fill", "#4694DD")
+      .style("stroke-width", "0.1")
+      .style("stroke", "black")
+      .on("mouseover", (_, d) => renderInfoBubble(d, groupsData))
+      .on("mousemove", () => {
+        d3.select(".classtip")
+          .style("top", event.pageY - 10 + "px")
+          .style("left", event.pageX + 170 + "px");
+      })
+      .on("mouseout", () => {
+        d3.select(".classtip").selectAll(".tip").remove();
+        d3.select(".classtip").style("visibility", "hidden");
+      });
+
+    oldGroupsData = groupsData;
+  }
+}
+*/
+export function renderSchedule(mapData, eventData, from) {
+  if (mapData.loading) return;
+
+  const g = d3.select(`.${gClass}`);
+  if (!isEqual(eventData, oldEventData)) {
+    if (!from) g.selectAll(".toDlt").remove();
+
+    var eventMapData = !isCourData(eventData)
+      ? filtreMapDataBy.groups(mapData, eventData)
+      : filtreMapDataBy.cour(mapData, eventData);
+
+    g.append("g")
+      .selectAll("path")
+      .data(eventMapData)
+      .enter()
+      .append("path")
+      .attr("class", "toDlt")
+      .attr("d", glob.path)
+      .style("fill", "#4694DD")
+      .style("stroke-width", "0.1")
+      .style("stroke", "black")
+      .on("mouseover", (_, d) => renderInfoBubble(d, eventData))
+      .on("mousemove", () => {
+        d3.select(".classtip")
+          .style("top", event.pageY - 10 + "px")
+          .style("left", event.pageX + 170 + "px");
+      })
+      .on("mouseout", () => {
+        d3.select(".classtip").selectAll(".tip").remove();
+        d3.select(".classtip").style("visibility", "hidden");
+      });
+
+    oldEventData = eventData;
   }
 }
